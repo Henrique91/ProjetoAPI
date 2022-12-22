d232b5a63e3401526f20ab82eed8e12b8855f86d
Henrique91 <henriquesilvacorrea91@gmail.com>
2022-12-22 15:43:15 -0300

    Atividade ER2 CRUD

---
+++ UC11/EncontroRemoto2_UC11/Chapter.WebApi/Chapter.WebApi/Controllers/LivroController.cs
@@ -0,0 +1,98 @@
+﻿using Chapter.WebApi.Models;
+using Chapter.WebApi.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapter.WebApi.Controllers
+{
+    [Produces("application/json")]//formato de resposta
+    [Route("api/[controller]")]//rota para acesso da api -- api/Livro
+    [ApiController]// que é um controller
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository; 
+        public LivroController(LivroRepository livroRepository) 
+        {
+            _livroRepository= livroRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Listar() 
+        {
+            try
+            {
+                return Ok(_livroRepository.Ler());
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Livro livro) 
+        {
+            try
+            {
+                _livroRepository.Cadastrar(livro);
+                return Ok(livro);
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Update(int id, Livro livro)
+        {
+            try
+            {
+                _livroRepository.Atualizar(id, livro);
+                return StatusCode(204);
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) 
+        {
+            try
+            {
+                _livroRepository.Deletar(id);
+                return StatusCode(204);           
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id) 
+        {
+            try
+            {
+                Livro livro = _livroRepository.BuscarPorId(id);
+
+                if (livro == null)
+                {
+                    return NotFound();
+                }
+                return Ok(livro);
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ UC11/EncontroRemoto2_UC11/Chapter.WebApi/Chapter.WebApi/Interfaces/ILivroRepository.cs
@@ -0,0 +1,17 @@
+﻿using Chapter.WebApi.Models;
+
+namespace Chapter.WebApi.Interfaces
+{
+    public interface ILivroRepository
+    {
+        List<Livro> Ler();
+
+        void Cadastrar(Livro livro);
+
+        void Atualizar(int id, Livro livro);
+
+        void Deletar(int id);
+
+        Livro BuscarPorId(int id);
+    }
+}
