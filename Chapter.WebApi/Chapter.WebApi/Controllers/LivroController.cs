9f9332ed0f1d5fa29968402295f982d0e43e830e
Henrique91 <henriquesilvacorrea91@gmail.com>
2022-12-22 10:42:06 -0300

    Atividade UC11 ER1

---
+++ Chapter.WebApi/Chapter.WebApi/Controllers/LivroController.cs
@@ -0,0 +1,32 @@
+﻿using Chapter.WebApi.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapter.WebApi.Controllers
+{
+    [Produces("application/json")]//formato de resposta
+    [Route("api/[controller]")]//rota para acesso da api -- api/Livro
+    [ApiController]// que é um controller
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository; 
+        public LivroController(LivroRepository livroRepository) 
+        {
+            _livroRepository= livroRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Listar() 
+        {
+            try
+            {
+                return Ok(_livroRepository.Ler());
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
